83ca41ae57425ad19ae383848bf88f92759438b4
Shaurya Gupta <84236073+shauryagupta45@users.noreply.github.com>
2023-05-07 23:57:39 +0530

    Update round_button.dart

--- lib/Widgets/round_button.dart
+++ lib/Widgets/round_button.dart
@@ -18,7 +18,7 @@ class RoundButton extends StatelessWidget {
       child: Container(
         height: 50,
         decoration: BoxDecoration(
-            color: Colors.deepPurple, borderRadius: BorderRadius.circular(10)),
+            color: Colors.deepPurple,borderRadius: BorderRadius.circular(10)),
         child: Center(
           child: loading
               ? const CircularProgressIndicator(
